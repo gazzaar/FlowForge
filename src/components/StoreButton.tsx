e6c7dcd91bc27ad4e94f8bd479332b59af06ceab
gazzaar <gazzarDev@gmail.com>
2025-10-14 09:03:52 +0300

    Make Button component

---
+++ src/components/StoreButton.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@mui/material";
+const StoreButton = ({ actionName, onAction }: { actionName: string }) => {
+  return (
+    <Button
+      variant="outlined"
+      onClick={onAction}
+      sx={{
+        color: "#86418D",
+        textTransform: "none",
+        fontSize: "16px",
+        borderColor: "#86418D",
+        bgcolor: "white",
+        p: "2px 10px",
+      }}
+    >
+      {actionName}
+    </Button>
+  );
+};
+
+export default StoreButton;
